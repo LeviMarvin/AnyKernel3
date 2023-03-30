8a193a8c38f08f1d33f481b51613a9c25dc7e5d0
Levi Marvin <levimarvin@icloud.com>
2023-03-31 01:23:33 +0800

    Update information for tenduo kernel.

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=lmperf kernel for Redmi K40 by Levi Marvin @ DEBUB BUILD
+kernel.string=lmperf tenduo kernel for Redmi K40 by Levi Marvin
 do.devicecheck=1
 do.modules=0
 do.systemless=1
